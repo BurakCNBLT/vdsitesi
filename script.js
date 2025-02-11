db9651c60743c6dea5b5b0ab4514e5a676955328
BurakCNBLT <81869269+BurakCNBLT@users.noreply.github.com>
2025-02-11 20:12:27 +0100

    Add files via upload
    oyunla birlesik

--- script.js
+++ script.js
@@ -24,3 +24,19 @@ function handleNoClick() {
 function handleYesClick() {
     window.location.href = "yes_page.html";
 }
+
+function handleContClick() {
+    window.location.href = "cont_page.html";
+}
+
+function handleReadyClick() {
+    window.location.href = "ready_page.html";
+}
+
+function handleNotreadyClick() {
+    window.location.href = "notready_page.html";
+}
+
+function handleOkClick() {
+    window.location.href = "https://layto.itch.io/prenses-esra";
+}
